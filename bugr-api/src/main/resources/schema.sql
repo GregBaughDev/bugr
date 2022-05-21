0a525e51af3d7f0dfeb72bfecabdd542fcdad3ca
Greg <80447308+GregBDesign@users.noreply.github.com>
2022-05-21 18:04:37 +1000

    update DB SQL

--- bugr-api/src/main/resources/schema.sql
+++ bugr-api/src/main/resources/schema.sql
@@ -14,3 +14,43 @@ CREATE TABLE users (
     is_confirmed BIT NOT NULL DEFAULT 0,
     date_registered DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 )
+
+DROP TABLE IF EXISTS reviews;
+CREATE TABLE reviews (
+    review_id INT AUTO_INCREMENT PRIMARY KEY,
+    author INT NOT NULL,
+    user INT NOT NULL,
+    date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    review VARCHAR(10000) NOT NULL,
+    FOREIGN KEY (author) REFERENCES users(user_id),
+    FOREIGN KEY (user) REFERENCES users(user_id)
+    )
+
+DROP TABLE IF EXISTS dates;
+CREATE TABLE dates (
+    date_id INT AUTO_INCREMENT PRIMARY KEY,
+    user INT NOT NULL,
+    date_from DATE NOT NULL,
+    date_to DATE NOT NULL,
+    FOREIGN KEY (user) REFERENCES users(user_id)
+    )
+
+DROP TABLE IF EXISTS chats;
+CREATE TABLE chats (
+    chat_id INT AUTO_INCREMENT PRIMARY KEY,
+    from_user INT NOT NULL,
+    to_user INT NOT NULL,
+    chat_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (from_user) REFERENCES users(user_id),
+    FOREIGN KEY (to_user) REFERENCES users(user_id)
+    )
+
+DROP TABLE IF EXISTS messages;
+CREATE TABLE messages (
+    message_id INT AUTO_INCREMENT PRIMARY KEY,
+    chat_id INT NOT NULL,
+    message VARCHAR(1000) NOT NULL,
+    message_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (chat_id) REFERENCES chats(chat_id)
+    )
+
