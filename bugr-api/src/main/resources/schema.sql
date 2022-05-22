93a1f8bf475bc3212ded33818d327b246e52a08a
Greg <80447308+GregBDesign@users.noreply.github.com>
2022-05-22 14:25:17 +1000

    update schema and seed data

--- bugr-api/src/main/resources/schema.sql
+++ bugr-api/src/main/resources/schema.sql
@@ -1,4 +1,3 @@
-DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(100) UNIQUE NOT NULL,
@@ -10,44 +9,43 @@ CREATE TABLE users (
     about_bug VARCHAR(10000) NOT NULL,
     is_confirmed BIT NOT NULL DEFAULT 0,
     date_registered DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
+);
 
-DROP TABLE IF EXISTS reviews;
 CREATE TABLE reviews (
     review_id INT AUTO_INCREMENT PRIMARY KEY,
     author INT NOT NULL,
     user INT NOT NULL,
     date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     review VARCHAR(10000) NOT NULL,
-    FOREIGN KEY (author) REFERENCES users(user_id),
-    FOREIGN KEY (user) REFERENCES users(user_id)
-    )
+    CONSTRAINT reviews_fkey_author FOREIGN KEY (author) REFERENCES users(user_id),
+    CONSTRAINT reviews_fkey_user FOREIGN KEY (user) REFERENCES users(user_id)
+    ON DELETE CASCADE
+);
 
-DROP TABLE IF EXISTS dates;
 CREATE TABLE dates (
     date_id INT AUTO_INCREMENT PRIMARY KEY,
     user INT NOT NULL,
     date_from DATE NOT NULL,
     date_to DATE NOT NULL,
-    FOREIGN KEY (user) REFERENCES users(user_id)
-    )
+    CONSTRAINT dates_fkey FOREIGN KEY (user) REFERENCES users(user_id)
+    ON DELETE CASCADE
+);
 
-DROP TABLE IF EXISTS chats;
 CREATE TABLE chats (
     chat_id INT AUTO_INCREMENT PRIMARY KEY,
     from_user INT NOT NULL,
     to_user INT NOT NULL,
     chat_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (from_user) REFERENCES users(user_id),
-    FOREIGN KEY (to_user) REFERENCES users(user_id)
-    )
+    CONSTRAINT chats_from_fkey FOREIGN KEY (from_user) REFERENCES users(user_id),
+    CONSTRAINT chats_to_fkey FOREIGN KEY (to_user) REFERENCES users(user_id)
+    ON DELETE CASCADE
+);
 
-DROP TABLE IF EXISTS messages;
 CREATE TABLE messages (
     message_id INT AUTO_INCREMENT PRIMARY KEY,
     chat_id INT NOT NULL,
     message VARCHAR(1000) NOT NULL,
     message_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (chat_id) REFERENCES chats(chat_id)
-    )
-
+    CONSTRAINT messages_chatid_fkey FOREIGN KEY (chat_id) REFERENCES chats(chat_id)
+    ON DELETE CASCADE
+);
