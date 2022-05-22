be4ef2610fcf33613d25b1e075122266ee3b6152
Greg <80447308+GregBDesign@users.noreply.github.com>
2022-05-22 14:49:55 +1000

    update schema and seed for postgres

--- bugr-api/src/main/resources/schema.sql
+++ bugr-api/src/main/resources/schema.sql
@@ -1,51 +1,53 @@
+CREATE TYPE state AS ENUM ('VIC', 'NSW', 'QLD', 'SA', 'WA', 'NT', 'TAS', 'ACT');
+CREATE TYPE user_type AS ENUM ('SEEKING', 'OFFERING', 'CARER');
 CREATE TABLE users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id SERIAL PRIMARY KEY,
     username VARCHAR(100) UNIQUE NOT NULL,
     user_password VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     location VARCHAR(100) NOT NULL,
-   	state ENUM('VIC', 'NSW', 'QLD', 'SA', 'WA', 'NT', 'TAS', 'ACT') NOT NULL,
-	user_type ENUM('SEEKING', 'OFFERING', 'CARER') NOT NULL,
+   	state state NOT NULL,
+	user_type user_type NOT NULL,
     about_bug VARCHAR(10000) NOT NULL,
-    is_confirmed BIT NOT NULL DEFAULT 0,
-    date_registered DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    is_confirmed BOOLEAN NOT NULL DEFAULT FALSE,
+    date_registered TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE reviews (
-    review_id INT AUTO_INCREMENT PRIMARY KEY,
+    review_id SERIAL PRIMARY KEY,
     author INT NOT NULL,
-    user INT NOT NULL,
-    date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    user_reviewed SERIAL NOT NULL,
+    date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     review VARCHAR(10000) NOT NULL,
     CONSTRAINT reviews_fkey_author FOREIGN KEY (author) REFERENCES users(user_id),
-    CONSTRAINT reviews_fkey_user FOREIGN KEY (user) REFERENCES users(user_id)
+    CONSTRAINT reviews_fkey_user FOREIGN KEY (user_reviewed) REFERENCES users(user_id)
     ON DELETE CASCADE
 );
 
 CREATE TABLE dates (
-    date_id INT AUTO_INCREMENT PRIMARY KEY,
-    user INT NOT NULL,
+    date_id SERIAL PRIMARY KEY,
+    user_dates INT NOT NULL,
     date_from DATE NOT NULL,
     date_to DATE NOT NULL,
-    CONSTRAINT dates_fkey FOREIGN KEY (user) REFERENCES users(user_id)
+    CONSTRAINT dates_fkey FOREIGN KEY (user_dates) REFERENCES users(user_id)
     ON DELETE CASCADE
 );
 
 CREATE TABLE chats (
-    chat_id INT AUTO_INCREMENT PRIMARY KEY,
+    chat_id SERIAL PRIMARY KEY,
     from_user INT NOT NULL,
     to_user INT NOT NULL,
-    chat_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    chat_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT chats_from_fkey FOREIGN KEY (from_user) REFERENCES users(user_id),
     CONSTRAINT chats_to_fkey FOREIGN KEY (to_user) REFERENCES users(user_id)
     ON DELETE CASCADE
 );
 
 CREATE TABLE messages (
-    message_id INT AUTO_INCREMENT PRIMARY KEY,
+    message_id SERIAL PRIMARY KEY,
     chat_id INT NOT NULL,
     message VARCHAR(1000) NOT NULL,
-    message_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    message_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT messages_chatid_fkey FOREIGN KEY (chat_id) REFERENCES chats(chat_id)
     ON DELETE CASCADE
 );
--- bugr-api/src/main/resources/seed.sql
+++ bugr-api/src/main/resources/seed.sql
@@ -1,35 +1,35 @@
-INSERT INTO users (`username`, `user_password`, `email`, `location`, `state`, `user_type`, `about_bug`, `is_confirmed`)
+INSERT INTO users (username, user_password, email, location, state, user_type, about_bug, is_confirmed)
 VALUES
-('test1', 'test1', 'test1@email.com', 'Melbourne', 'VIC', 'SEEKING', 'Sourdough bug, low maintenance. Feed once a week or whenever used', 1),
-('test2', 'test2', 'test2@email.com', 'Sydney', 'NSW', 'OFFERING', 'Sourdough bug, low maintenance. Feed once a week or whenever used', 1),
-('test3', 'test3', 'test3@email.com', 'Adelaide', 'SA', 'CARER', 'Sourdough bug, low maintenance. Feed once a week or whenever used', 1),
-('test4', 'test4', 'test4@email.com', 'Perth', 'WA', 'SEEKING', 'Sourdough bug, low maintenance. Feed once a week or whenever used', 1),
-('test5', 'test5', 'test5@email.com', 'Darwin', 'NT', 'OFFERING', 'Sourdough bug, low maintenance. Feed once a week or whenever used', 1),
-('test6', 'test6', 'test6@email.com', 'Brisbane', 'QLD', 'CARER', 'Sourdough bug, low maintenance. Feed once a week or whenever used', 0),
-('test7', 'test7', 'test7@email.com', 'Hobart', 'TAS', 'SEEKING', 'Sourdough bug, low maintenance. Feed once a week or whenever used', 0);
+('test1', 'test1', 'test1@email.com', 'Melbourne', 'VIC', 'SEEKING', 'Sourdough bug, low maintenance. Feed once a week or whenever used', true),
+('test2', 'test2', 'test2@email.com', 'Sydney', 'NSW', 'OFFERING', 'Sourdough bug, low maintenance. Feed once a week or whenever used', true),
+('test3', 'test3', 'test3@email.com', 'Adelaide', 'SA', 'CARER', 'Sourdough bug, low maintenance. Feed once a week or whenever used', true),
+('test4', 'test4', 'test4@email.com', 'Perth', 'WA', 'SEEKING', 'Sourdough bug, low maintenance. Feed once a week or whenever used', true),
+('test5', 'test5', 'test5@email.com', 'Darwin', 'NT', 'OFFERING', 'Sourdough bug, low maintenance. Feed once a week or whenever used', true),
+('test6', 'test6', 'test6@email.com', 'Brisbane', 'QLD', 'CARER', 'Sourdough bug, low maintenance. Feed once a week or whenever used', false),
+('test7', 'test7', 'test7@email.com', 'Hobart', 'TAS', 'SEEKING', 'Sourdough bug, low maintenance. Feed once a week or whenever used', false);
 
-INSERT INTO reviews (`author`, `user`, `review`)
+INSERT INTO reviews (author, user_reviewed, review)
 VALUES
 (1, 2, 'Super friendly and generous with their bug'),
 (3, 4, 'Willing to provide me with heaps of their bug'),
 (5, 6, 'They done a great job of looking after my bug and it came back to me in a great state'),
 (6, 7, 'Avoid. Unhelpful and unfriendly. Their bug was of low quality and smelt awful!');
 
-INSERT INTO dates (`user`, `date_from`, `date_to`)
+INSERT INTO dates (user_dates, date_from, date_to)
 VALUES
 (1, '2022-07-01', '2022-07-10'),
 (2, '2022-08-01', '2022-08-07'),
 (3, '2022-09-10', '2022-09-21'),
 (4, '2022-10-01', '2022-10-30');
 
-INSERT INTO chats (`from_user`, `to_user`)
+INSERT INTO chats (from_user, to_user)
 VALUES
 (1, 2),
 (3, 4),
 (1, 6),
 (3, 4);
 
-INSERT INTO messages (`chat_id`, `message`)
+INSERT INTO messages (chat_id, message)
 VALUES
 (1, 'Hi there'),
 (1, 'Hey - how`s it going?'),
