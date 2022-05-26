2bd44fb1164fbbd65d5d855df6499ad412d0c09e
Greg <80447308+GregBDesign@users.noreply.github.com>
2022-05-26 20:33:14 +1000

    add annotations to review model

--- bugr-api/src/main/kotlin/com/bugr/api/bugrapi/models/Review.kt
+++ bugr-api/src/main/kotlin/com/bugr/api/bugrapi/models/Review.kt
@@ -1,12 +1,22 @@
 package com.bugr.api.bugrapi.models
 
 import java.util.Date
+import javax.persistence.*
 
-class Review (
+@Entity
+@Table(name="REVIEWS")
+class Reviews (
+    @Id
+    @Column(name="REVIEW_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Int,
+    @Column(name="USER_REVIEWED")
     var user: Int,
+    @Column(name="AUTHOR")
     var author: Int,
+    @Column(name="DATE")
     var date: Date,
+    @Column(name="REVIEW")
     var review: String
     ){
 //    TO DO: User and Author will both be FK
