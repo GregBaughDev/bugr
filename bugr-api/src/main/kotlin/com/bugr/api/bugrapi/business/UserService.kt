ee771f18ac921d6c0ae7d311cbfbff32f6458e0f
Greg <80447308+GregBDesign@users.noreply.github.com>
2022-07-19 20:24:49 +1000

    fix email regex

--- bugr-api/src/main/kotlin/com/bugr/api/bugrapi/business/UserService.kt
+++ bugr-api/src/main/kotlin/com/bugr/api/bugrapi/business/UserService.kt
@@ -9,22 +9,17 @@ import org.springframework.stereotype.Service
 @Service
 class UserService(private val userRepository: UserRepository) {
 
+    private val emailRegex: Regex = "\\b[\\w.-]+@[\\w.-]+\\.\\w{2,4}\\b".toRegex()
+
     fun userLogin(username: String, password: String): LoggedInUser {
         if(username.isEmpty() || password.isEmpty()) throw InvalidInputException()
         return userRepository.userLogin(username, password)
     }
 
     fun newUser(newUser: Users): Users {
-        println(newUser.email)
-        println(newUser.userPassword)
-        // TO DO WIP -> Below regex doesnt work
         if (!emailRegex.matches(newUser.email)) throw InvalidInputException()
-        // if (!Regex("/^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}\$/gm").matches(newUser.userPassword)) throw InvalidInputException()
         // ENCRYPT PASSWORD
-
+        // CHECK IF USER EXISTS
         return userRepository.save(newUser)
     }
-
-    private val emailRegex: Regex = "/\b[\\w.-]+@[\\w.-]+\\.\\w{2,4}\b/gi".toRegex()
-
 }
