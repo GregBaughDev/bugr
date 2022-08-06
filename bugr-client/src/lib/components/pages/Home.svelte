c453496befd7813a6322d920b7cf1c13851bc76b
Greg <80447308+GregBDesign@users.noreply.github.com>
2022-08-06 16:56:47 +1000

    add skeleton page components

---
+++ bugr-client/src/lib/components/pages/Home.svelte
@@ -0,0 +1,12 @@
+<div class="flex flex-row items-center justify-around">
+  <div class="w-2/5">
+    <img class="w-full" src="src/assets/bugr_logo.svg" alt="bugr logo">
+  </div>
+  <div class="w-2/5">
+    <p><span class="font-serif">bugr</span> is a social network for people looking for sourdough bugs, carers for their sourdough bugs and those willing to look after other peoples bugs.</p>
+    <br>
+    <p>make an account and list your bug or reach out to others who are advertising theirs. If you have space in your fridge, perhaps you could look after someone elses bug.</p>
+    <br>
+    <p>picture the scene -> you're about to go on your holidays. You're all packed and then you remember your bug. "Oh no", you think, well <span class="font-serif">bugr</span> is here at your rescue.</p>
+  </div>
+</div>
