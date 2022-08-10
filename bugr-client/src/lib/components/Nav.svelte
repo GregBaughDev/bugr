91c6a1021ae762b512e58a38ae638f2265133fef
Greg <80447308+GregBDesign@users.noreply.github.com>
2022-08-10 20:14:27 +1000

    add filter for nav items

--- bugr-client/src/lib/components/Nav.svelte
+++ bugr-client/src/lib/components/Nav.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
-  import { currentPage } from '../state/globalStore'
+  import { currentPage, userLoggedIn } from '../state/globalStore'
   import { navItems, NavLinks } from '../types/types'
-  
+  // ADD A FILTER FOR LOGIN VARIABLE AND DISPLAY LOGIN/OUT ACCORDINGLY
   const handleClick = (link: NavLinks): void => {
     $currentPage = link
+  }
+
+  const filterNavItems = (navItems: {link: NavLinks, auth: boolean}[]): {link: NavLinks, auth: boolean}[] => {
+    return navItems.filter((item) => item.auth === $userLoggedIn || [NavLinks.Home, NavLinks.About, NavLinks.Rules].includes(item.link))
   }
 </script>
 
 <div class="flex flex-col w-2/12">
-  {#each navItems as nav }
-    <h3 on:click={() => handleClick(nav)}>{nav}</h3>
+  {#each filterNavItems(navItems) as nav }
+    <h3 on:click={() => handleClick(nav.link)}>{nav.link}</h3>
   {/each}
 </div>
