18fe301c2b775b6fc89b6fd1461c0fc8709251e1
Greg <80447308+GregBDesign@users.noreply.github.com>
2022-08-17 20:33:13 +1000

    WIP: auth routes

--- bugr-api/src/main/kotlin/com/bugr/api/bugrapi/v1/api/UserController.kt
+++ bugr-api/src/main/kotlin/com/bugr/api/bugrapi/v1/api/UserController.kt
@@ -4,22 +4,40 @@ import com.bugr.api.bugrapi.business.UserService
 import com.bugr.api.bugrapi.models.LoggedInUser
 import com.bugr.api.bugrapi.models.LoginForm
 import com.bugr.api.bugrapi.models.Users
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.Cookie
+import javax.servlet.http.HttpServletResponse
 
+@CrossOrigin(
+    origins = ["*"],
+    methods = [RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS],
+    allowedHeaders = ["*"])
 @RestController
 @RequestMapping("/api/v1")
 class UserController(private val userService: UserService) {
 
     @PostMapping("/users", consumes = [MediaType.APPLICATION_JSON_VALUE])
-    fun userLogin(@RequestBody login: LoginForm): LoggedInUser? {
+    fun userLogin(@RequestBody login: LoginForm, response: HttpServletResponse): LoggedInUser? {
+        // LOOK AT RESPONSE ENTITY
+        // SORT OUT CROSS ORIGIN ISSUES - MOVE METHODS TO TRY CATCH AND WORK OUT HOW TO DEAL WITH RESPONSE ON CLIENT
+        // MOVE RETRIEVE INFORMATION TO ANOTHER METHOD AND JUST SET A HEADER WHICH CAN THEN RETRIEVE INFORMATION TO DISPLAY TO USER
+        // ADD AUTH HEADER COOKIE
+        var cookie: Cookie = Cookie("test", "Greg")
+        response.addCookie(cookie)
         return userService.userLogin(login.username, login.password)
     }
 
--- bugr-client/src/api/users.ts
+++ bugr-client/src/api/users.ts
@@ -1,8 +1,13 @@
 import type { UserLogin } from "./types"
 import { ApiRoutesUsers } from "./types"
 
-export const userLogin = async (data: UserLogin): Promise<void> => {
-  const loginCall = await fetch(ApiRoutesUsers.LOGIN, { method: 'POST', body: JSON.stringify(data) })
-  const result = await loginCall.json()
-  return result
+export const userLogin = async (data): Promise<void> => {
+  try {
+    const loginCall = await fetch(ApiRoutesUsers.LOGIN, { method: 'POST', headers: { 'Content-Type': 'application/json' }, mode: 'cors', body: JSON.stringify(data) })
+    const result = await loginCall.json()
+    console.log({result})
+    return result
+  } catch (e) {
+    console.log({ e })
+  }
 }
