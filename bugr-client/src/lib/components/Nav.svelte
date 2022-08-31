d8cb5b9b56b70ec9b6b87d7ade705436a7900b81
Greg <80447308+GregBDesign@users.noreply.github.com>
2022-08-31 20:22:26 +1000

    revert nav sort fn

--- bugr-client/src/lib/components/Nav.svelte
+++ bugr-client/src/lib/components/Nav.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
-  import { navItems } from '../types/types'
-</script>
+  import { navItems, NavLinks } from '../types/types'
+  import type { NavItem } from '../types/types';
+  import { userLoggedIn } from '../state/globalStore'
 
+  const filterNavItems = (navItems: NavItem[]): NavItem[] => {
+    return navItems.filter((item) => item.auth === $userLoggedIn || [NavLinks.Home, NavLinks.About, NavLinks.Rules].includes(item.link))
+  }
+</script>
+<!-- LOOK INTO PREVENT DEFAULT ON REFRESH -->
 <div class="mr-10">
   <nav class="flex flex-col w-2/12">
-    {#each navItems as nav }
-      <a href={`/#${nav.route}`}>{nav.link}</a>
-    {/each}
+    {#key $userLoggedIn}
+      {#each filterNavItems(navItems) as nav }
+        <a href={`/#${nav.route}`}>{nav.link}</a>
+      {/each}
+    {/key}
   </nav>
 </div>
