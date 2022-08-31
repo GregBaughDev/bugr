af01f1e083ca9efc62b7f589190579e5a174db5c
Greg <80447308+GregBDesign@users.noreply.github.com>
2022-08-31 20:23:03 +1000

    add auth for protected routes

--- bugr-client/src/App.svelte
+++ bugr-client/src/App.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+  import Router, { push } from 'svelte-spa-router'
+  import wrap from 'svelte-spa-router/wrap'
   import './app.css'
   import Nav from './lib/components/Nav.svelte';
-  import Router from 'svelte-spa-router'
   import Home from './lib/components/pages/Home.svelte'
   import About from './lib/components/pages/About.svelte'
   import Login from './lib/components/pages/Login.svelte'
@@ -10,15 +11,39 @@
   import Rules from './lib/components/pages/Rules.svelte'
   import Search from './lib/components/pages/Search.svelte'
   import Fallback from './lib/components/pages/Fallback.svelte';
+  import { userLoggedIn } from '../src/lib/state/globalStore'
+
+  const checkUserAuthOrRedirect = (loggedIn: boolean): boolean => {
+    if ($userLoggedIn) {
+      return true
+    } else {
+      push('/login')
+    }
+  }
 
   const routes = {
     '/': Home,
     '/about': About,
     '/login': Login,
-    '/messages': Messages,
-    '/profile': Profile,
+    '/messages': wrap({
+      component: Messages,
+      conditions: [
+        () => checkUserAuthOrRedirect($userLoggedIn)
+      ]
+    }),
+    '/profile': wrap({
+      component: Profile,
+      conditions: [
+        () => checkUserAuthOrRedirect($userLoggedIn)
+      ]
+    }),
     '/rules': Rules,
-    '/search': Search,
+    '/search': wrap({
+      component: Search,
+      conditions: [
+        () => checkUserAuthOrRedirect($userLoggedIn)
+      ]
+    }),
     '*': Fallback
   }
 </script>
