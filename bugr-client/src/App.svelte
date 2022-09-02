9d69974f5b21413183e8bb629a2096817f004fd9
Greg <80447308+GregBDesign@users.noreply.github.com>
2022-09-02 20:16:42 +1000

    move routes to own file

--- bugr-client/src/App.svelte
+++ bugr-client/src/App.svelte
@@ -1,58 +1,8 @@
 <script lang="ts">
   import Router, { push } from 'svelte-spa-router'
-  import wrap from 'svelte-spa-router/wrap'
+  import { routes } from './routes'
   import './app.css'
-  import Nav from './lib/components/Nav.svelte';
-  import Home from './lib/components/pages/Home.svelte'
-  import About from './lib/components/pages/About.svelte'
-  import Login from './lib/components/pages/Login.svelte'
-  import Logout from './lib/components/pages/Logout.svelte'
-  import Messages from './lib/components/pages/Messages.svelte'
-  import Profile from './lib/components/pages/Profile.svelte'
-  import Rules from './lib/components/pages/Rules.svelte'
-  import Search from './lib/components/pages/Search.svelte'
-  import Fallback from './lib/components/pages/Fallback.svelte';
-  import { userLoggedIn } from '../src/lib/state/globalStore'
-
-  const checkUserAuthOrRedirect = (loggedIn: boolean): boolean => {
-    if ($userLoggedIn) {
-      return true
-    } else {
-      push('/login')
-    }
-  }
-
-  const routes = {
-    '/': Home,
-    '/about': About,
-    '/login': Login,
-    '/logout': wrap({
-      component: Logout,
-      conditions: [
-        () => checkUserAuthOrRedirect($userLoggedIn)
-      ]
-    }),
-    '/messages': wrap({
-      component: Messages,
-      conditions: [
-        () => checkUserAuthOrRedirect($userLoggedIn)
-      ]
-    }),
-    '/profile': wrap({
-      component: Profile,
-      conditions: [
-        () => checkUserAuthOrRedirect($userLoggedIn)
-      ]
-    }),
-    '/rules': Rules,
-    '/search': wrap({
-      component: Search,
-      conditions: [
-        () => checkUserAuthOrRedirect($userLoggedIn)
-      ]
-    }),
-    '*': Fallback
-  }
+  import Nav from './lib/components/Nav.svelte';  
 </script>
 
 <main class="bg-background h-screen w-screen">
---
+++ bugr-client/src/routes.ts
@@ -0,0 +1,55 @@
+import { get } from 'svelte/store'
+import { push } from 'svelte-spa-router'
+import wrap from 'svelte-spa-router/wrap'
+import Home from './lib/components/pages/Home.svelte'
+import About from './lib/components/pages/About.svelte'
+import Login from './lib/components/pages/Login.svelte'
+import Logout from './lib/components/pages/Logout.svelte'
+import Messages from './lib/components/pages/Messages.svelte'
+import Profile from './lib/components/pages/Profile.svelte'
+import Rules from './lib/components/pages/Rules.svelte'
+import Search from './lib/components/pages/Search.svelte'
+import Signup from './lib/components/pages/Signup.svelte';
+import Fallback from './lib/components/pages/Fallback.svelte';
+import { userLoggedIn } from '../src/lib/state/globalStore'
+
+const checkUserAuthOrRedirect = (loggedIn: boolean): boolean => {
+    if (loggedIn) {
+      return true
+    } else {
+      push('/login')
+    }
+  }
+
+export const routes = {
+    '/': Home,
+    '/about': About,
+    '/login': Login,
+    '/logout': wrap({
+      component: Logout,
+      conditions: [
+        () => checkUserAuthOrRedirect(get(userLoggedIn))
+      ]
+    }),
+    '/messages': wrap({
+      component: Messages,
+      conditions: [
+        () => checkUserAuthOrRedirect(get(userLoggedIn))
+      ]
+    }),
+    '/profile': wrap({
+      component: Profile,
+      conditions: [
+        () => checkUserAuthOrRedirect(get(userLoggedIn))
+      ]
+    }),
+    '/rules': Rules,
+    '/search': wrap({
+      component: Search,
+      conditions: [
+        () => checkUserAuthOrRedirect(get(userLoggedIn))
+      ]
+    }),
+    '/signup': Signup,
+    '*': Fallback
+  }
