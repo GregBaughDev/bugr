153ce4f11076d88faefeacbd17095f1bb13714ee
Greg <80447308+GregBDesign@users.noreply.github.com>
2022-09-06 21:17:27 +1000

    update logout

--- bugr-client/src/lib/components/pages/Logout.svelte
+++ bugr-client/src/lib/components/pages/Logout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import { push } from 'svelte-spa-router'
   import { userLoggedIn } from '../../state/globalStore'
+  import { userDetails } from '../../state/userStore'
 
+  $userDetails = null
   $userLoggedIn = false
   push('/')
 </script>
