6967fbcc44b5511eea0d28743fdad95331b0de93
Greg <80447308+GregBDesign@users.noreply.github.com>
2022-09-10 15:13:44 +1000

    cleanup imports

--- bugr-api/src/main/kotlin/com/bugr/api/bugrapi/v1/api/UserController.kt
+++ bugr-api/src/main/kotlin/com/bugr/api/bugrapi/v1/api/UserController.kt
@@ -4,23 +4,16 @@ import com.bugr.api.bugrapi.business.UserService
 import com.bugr.api.bugrapi.models.LoggedInUser
 import com.bugr.api.bugrapi.models.LoginForm
 import com.bugr.api.bugrapi.models.Users
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import javax.servlet.http.Cookie
-import javax.servlet.http.HttpServletResponse
 
 @CrossOrigin(
     origins = ["*"],
