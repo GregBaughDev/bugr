104847a5b69a923cd7bce7211656bcbc6bac0a04
Greg <80447308+GregBDesign@users.noreply.github.com>
2022-09-10 15:12:29 +1000

    add new user route

--- bugr-client/src/api/types.ts
+++ bugr-client/src/api/types.ts
@@ -1,10 +1,20 @@
-export enum ApiRoutesUsers {
-  LOGIN = 'http://localhost:8080/api/v1/users',
-}
-// REPLACE ABOVE WITH A BASE
+import type { User } from "src/lib/types/types"
+
+export const ApiRoutesUsers = {
+  BASE: 'http://localhost:8080/api/v1',
+  LOGIN: '/users',
+  NEW_USER: '/users/new'
+} as const
+
 export interface UserLogin {
   body: {
     username: string
     password: string
+  }
+}
+
+export interface NewUser {
+  body: {
+    newUser: User
   }
 }
