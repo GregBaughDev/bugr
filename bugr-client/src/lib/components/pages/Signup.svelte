d79625fb6c88ed251880ab331c52b5dab3cb7116
Greg <80447308+GregBDesign@users.noreply.github.com>
2022-09-12 20:30:46 +1000

    update form styles

--- bugr-client/src/lib/components/pages/Signup.svelte
+++ bugr-client/src/lib/components/pages/Signup.svelte
@@ -20,45 +20,45 @@
 <!-- TODO: ADD REQUIRED FIELDS CHECKS -->
 <div class="w-10/12 p-4">
   <h2>Sign up</h2>
-  <form>
-    <div>
+  <form class="flex flex-col w-full items-center mt-8">
+    <div class="mt-4 flex justify-between w-full items-center">
       <label for="username">Enter a username:</label>
-      <input type="text" name="username" id="username" bind:value={newUserForm.username}>
+      <input type="text" name="username" id="username" class="w-2/3 p-2" bind:value={newUserForm.username}>
     </div>
-    <div>
+    <div class="mt-2 flex justify-between w-full items-center">
       <label for="password">Enter a password:</label>
-      <input type="password" name="password" id="password" bind:value={newUserForm.password}>
+      <input type="password" name="password" id="password" class="w-2/3 p-2" bind:value={newUserForm.password}>
     </div>
-    <div>
+    <div class="mt-2 flex justify-between w-full items-center">
       <label for="email">Enter your email:</label>
-      <input type="email" name="email" id="email" bind:value={newUserForm.email}>
+      <input type="email" name="email" id="email" class="w-2/3 p-2" bind:value={newUserForm.email}>
     </div>
-    <div>
+    <div class="mt-2 flex justify-between w-full items-center">
       <label for="location">Enter your location:</label>
-      <input type="text" name="location" id="location" bind:value={newUserForm.location}>
+      <input type="text" name="location" id="location" class="w-2/3 p-2" bind:value={newUserForm.location}>
     </div>
-    <div>
+    <div class="mt-2 flex justify-between w-full items-center">
       <label for="state">Select your state:</label>
-      <select name="state" id="state" bind:value={newUserForm.state}>
+      <select name="state" id="state" class="w-2/3 p-2" bind:value={newUserForm.state}>
         {#each Object.values(States) as state}
           <option value={state}>{state}</option>
         {/each}
       </select>
     </div>
-    <div>
+    <div class="mt-2 flex justify-between w-full items-center">
       <label for="userType">User type:</label>
-      <select name="userType" id="userType" required bind:value={newUserForm.userType}>
+      <select name="userType" id="userType"class="w-2/3 p-2" bind:value={newUserForm.userType}>
         {#each Object.entries(UserType) as [key, value]}
           <option value={key}>{value}</option>
         {/each}
       </select>
     </div>
-    <div>
+    <div class="mt-2 flex justify-between w-full items-center">
       <label for="about">About:</label>
-      <textarea id="about" name="about" required bind:value={newUserForm.aboutBug}></textarea>
+      <textarea id="about" name="about" class="w-2/3 resize-none p-2" bind:value={newUserForm.aboutBug}></textarea>
     </div>
-    <div>
-      <button type="button" on:click={handleSubmit}>Sign up!</button>
+    <div class="mt-4 flex items-center justify-center w-full">
+      <button type="button" class="border-white border-2 p-4 rounded-xl w-1/3 font-bold" on:click={handleSubmit}>Sign up!</button>
     </div>
   </form>
 </div>
