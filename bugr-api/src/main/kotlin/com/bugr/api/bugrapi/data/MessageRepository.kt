0c285b85653efffcaf60bccfab6a07d81fecea78
Greg <80447308+GregBDesign@users.noreply.github.com>
2022-09-26 20:37:16 +1000

    breaking change: attempt to fix sql array types

--- bugr-api/src/main/kotlin/com/bugr/api/bugrapi/data/MessageRepository.kt
+++ bugr-api/src/main/kotlin/com/bugr/api/bugrapi/data/MessageRepository.kt
@@ -4,7 +4,6 @@ import com.bugr.api.bugrapi.models.Messages
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
-import java.util.Optional
 
 @Repository
 interface MessageRepository : JpaRepository<Messages, Int> {
