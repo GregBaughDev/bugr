b199a57adc7ad9b83757a62bd05dabb0a12f4f7b
Greg <80447308+GregBDesign@users.noreply.github.com>
2022-10-12 20:47:36 +1100

    search page wip

--- bugr-client/src/lib/components/pages/Search.svelte
+++ bugr-client/src/lib/components/pages/Search.svelte
@@ -1,3 +1,8 @@
+<script lang='ts'>
+  // Search for users by location
+  // be able to click on a user page and display their user page show dates and reviews
+</script>
+
 <div class="w-10/12 p-4">
   <h2>Search</h2>
 </div>
