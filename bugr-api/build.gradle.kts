22bd8f0299d2c3802866eb7ce9c08385ca4f1d04
Greg <80447308+GregBDesign@users.noreply.github.com>
2023-02-03 20:27:11 +1100

    Update build.gradle

--- bugr-api/build.gradle.kts
+++ bugr-api/build.gradle.kts
@@ -20,7 +20,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.security:spring-security-core")
-//	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.kafka:spring-kafka")
 	implementation("com.vladmihalcea:hibernate-types-55:2.19.2")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
