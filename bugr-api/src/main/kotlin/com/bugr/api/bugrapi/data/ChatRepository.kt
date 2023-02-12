350f13e3780b3c53b10210040a4d424e2fa96e85
Greg <80447308+GregBDesign@users.noreply.github.com>
2023-02-12 20:13:10 +1100

    Add chat service and repository

--- bugr-api/src/main/kotlin/com/bugr/api/bugrapi/data/ChatRepository.kt
+++ bugr-api/src/main/kotlin/com/bugr/api/bugrapi/data/ChatRepository.kt
@@ -1,18 +1,18 @@
-//package com.bugr.api.bugrapi.data
-//
-//import com.bugr.api.bugrapi.models.Chats
-//import org.springframework.data.jpa.repository.JpaRepository
-//import org.springframework.data.jpa.repository.Query
-//import org.springframework.stereotype.Repository
-//import java.util.Optional
-//
-//@Repository
-//interface ChatRepository : JpaRepository<Chats, Int> {
-//
-//    @Query(
-//        value = "SELECT chats.*, users.username FROM chats INNER JOIN users ON chats.from_user = users.user_id WHERE to_user = :id",
-//        nativeQuery = true
-//    )
-//    fun getAllChatsForUser(id: Int): Optional<Chats>
-//
-//}
+package com.bugr.api.bugrapi.data
+
+import com.bugr.api.bugrapi.models.Chat
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
+import java.util.Optional
+
+@Repository
+interface ChatRepository : JpaRepository<Chat, Int> {
+
+    @Query(
+        value = "SELECT * FROM chats WHERE to_user = :id OR from_user = :id",
+        nativeQuery = true
+    )
+    fun getAllChatsForUser(id: Int): Optional<List<Chat>>
+
+}
